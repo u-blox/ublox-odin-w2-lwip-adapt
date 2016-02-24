00a17e4bd8a64c59c583b9989da62bb90fdf3d14
andreas.larsson <andreas.larsson@connectblue.com>
2016-02-24 15:04:35 +0100

    Added PAN interface

--- source/cb_ip_panif.c
+++ source/cb_ip_panif.c
@@ -15,10 +15,15 @@
 * limitations under the License.
 */
 
-#define __CB_FILE__ "cbIP_WLAN_IF"
+#define __CB_FILE__ "cbIP_PAN_IF"
+
+#include <string.h>
+#include <stdio.h>
 
 #include "cb_ip.h"
-#include "cb_wlan.h"
+#include "cb_ip_buf.h"
+#include "cb_bt_pan.h"
+#include "cb_bt_conn_man.h"
 #include "cb_log.h"
 
 #include "lwip/netif.h"
@@ -29,7 +34,8 @@
 #include "lwip/stats.h"
 #include "lwip/dns.h"
 
-#include <string.h>
+#include "ualloc/ualloc.h"
+
 
 /*===========================================================================
  * DEFINES
@@ -41,10 +47,10 @@
 #define LWIP_PRINT(...)
 #endif
 
-#define IFNAME0 'w'
-#define IFNAME1 'l'
+#define IFNAME0 'b'
+#define IFNAME1 'p'
 
-#define IFNAME "wl0"
+#define IFNAME "bp0"
 
 /*===========================================================================
  * TYPES
@@ -55,37 +61,48 @@ typedef struct {
     cbIP_interfaceSettings ifConfig;
     cbIP_statusIndication statusCallback;
     void* callbackArg;
-} cbIP_wlanIf;
+} cbIP_panIf;
 
 typedef enum {
-    cbIP_WLAN_LINKUP,
-    cbIP_WLAN_LINKDOWN,
-} cbIP_WLAN_LinkState;
+    cbIP_PAN_LINKUP,
+    cbIP_PAN_LINKDOWN,
+} cbIP_PAN_LinkState;
 
 
 /*===========================================================================
  * DECLARATIONS
  *=========================================================================*/
 static err_t cb_netif_init(struct netif* netif);
-static err_t wlanif_output_ipv4(struct netif* netif, struct pbuf* p, struct ip_addr* ipaddr);
-static err_t wlanif_output_ipv6(struct netif *netif, struct pbuf *p, ip6_addr_t *ipaddr);
+static err_t panif_output_ipv4(struct netif* netif, struct pbuf* p, struct ip_addr* ipaddr);
+static err_t panif_output_ipv6(struct netif *netif, struct pbuf *p, ip6_addr_t *ipaddr);
 
 static err_t low_level_output(struct netif* netif, struct pbuf* p);
 static void netif_status_callback(struct netif *netif);
 
-static void statusIndication(void *callbackContext, cbWLAN_StatusIndicationInfo status, void *data);
-static void packetIndication(void *callbackContext, cbWLAN_PacketIndicationInfo *packetInfo);
+static void handleConnectEvt(cbBCM_Handle connHandle, cbBCM_ConnectionInfo info);
+static void handleDisconnectEvt(cbBCM_Handle connHandle);
+static void handleDataEvt(cbBTPAN_Handle btPanHandle, cb_uint8* pData, cb_uint16 length);
+static void handleDataCnf(cbBTPAN_Handle btPanHandle, cb_int32 result);
 
 /*===========================================================================
  * DEFINITIONS
  *=========================================================================*/
 
-cbIP_wlanIf wlanIf;
+cbIP_panIf panIf;
+
+static const cbBTPAN_Callback _panCallBack =
+{
+    handleConnectEvt,
+    handleDisconnectEvt,
+    handleDataEvt,
+    handleDataCnf,
+};
+
 /*===========================================================================
  * FUNCTIONS
  *=========================================================================*/
 
-void cbIP_initWlanInterfaceStatic(char* hostname, const cbIP_IPv4Settings * const IPv4Settings, const cbIP_IPv6Settings * const IPv6Settings, cbIP_interfaceSettings const * const ifConfig, cbIP_statusIndication callback, void* callbackArg)
+void cbIP_initPanInterfaceStatic(char* hostname, const cbIP_IPv4Settings * const IPv4Settings, const cbIP_IPv6Settings * const IPv6Settings, cbIP_interfaceSettings const * const ifConfig, cbIP_statusIndication callback, void* callbackArg)
 {
     struct ip_addr ipaddr;
     struct ip_addr netmask;
@@ -104,23 +121,23 @@ void cbIP_initWlanInterfaceStatic(char* hostname, const cbIP_IPv4Settings * cons
 
     memcpy(&ip6addr, &IPv6Settings->linklocal.value, sizeof(ip6addr));
 
-    memcpy(&wlanIf.ifConfig, ifConfig, sizeof(wlanIf.ifConfig));
-    wlanIf.statusCallback = callback;
-    wlanIf.callbackArg = callbackArg;
-    netif_add(&wlanIf.hInterface, &ipaddr, &netmask, &gw, &wlanIf, cb_netif_init, ethernet_input);
-    wlanIf.hInterface.hostname = hostname;
+    memcpy(&panIf.ifConfig, ifConfig, sizeof(panIf.ifConfig));
+    panIf.statusCallback = callback;
+    panIf.callbackArg = callbackArg;
+    netif_add(&panIf.hInterface, &ipaddr, &netmask, &gw, &panIf, cb_netif_init, ethernet_input);
+    panIf.hInterface.hostname = hostname;
 
-    wlanIf.hInterface.ip6_autoconfig_enabled = 0;
+    panIf.hInterface.ip6_autoconfig_enabled = 0;
     if ((ip6addr.addr[0] == 0) && (ip6addr.addr[1] == 0) &&
         (ip6addr.addr[2] == 0) && (ip6addr.addr[3] == 0))
     {
-        netif_create_ip6_linklocal_address(&wlanIf.hInterface, 1);
+        netif_create_ip6_linklocal_address(&panIf.hInterface, 1);
     }
     else
     {
-        memcpy(&wlanIf.hInterface.ip6_addr[0], &ip6addr, sizeof(ip6addr));
+        memcpy(&panIf.hInterface.ip6_addr[0], &ip6addr, sizeof(ip6addr));
     }
-    netif_ip6_addr_set_state((&wlanIf.hInterface), 0, IP6_ADDR_TENTATIVE);
+    netif_ip6_addr_set_state((&panIf.hInterface), 0, IP6_ADDR_TENTATIVE);
 
     dns_setserver(0, &dns0);
     dns_setserver(1, &dns1);
@@ -142,13 +159,17 @@ void cbIP_initWlanInterfaceStatic(char* hostname, const cbIP_IPv4Settings * cons
                ip4_addr1(&dns1), ip4_addr2(&dns1),
                ip4_addr3(&dns1), ip4_addr4(&dns1));
 
-    wlanIf.statusCallback = callback;
-    wlanIf.hInterface.state = &wlanIf;
-    netif_set_status_callback(&wlanIf.hInterface, netif_status_callback);
-    netif_set_up(&wlanIf.hInterface);
+    panIf.statusCallback = callback;
+    panIf.hInterface.state = &panIf;
+    netif_set_status_callback(&panIf.hInterface, netif_status_callback);
+    netif_set_up(&panIf.hInterface);
+
+    cb_uint32 result;
+    result = cbBTPAN_registerDataCallback(&_panCallBack);
+    cb_ASSERT(result == cbBTPAN_RESULT_OK);
 }
 
-void cbIP_initWlanInterfaceDHCP(char* hostname, const cbIP_IPv6Settings * const IPv6Settings, cbIP_interfaceSettings const * const ifConfig, cbIP_statusIndication callback, void* callbackArg)
+void cbIP_initPanInterfaceDHCP(char* hostname, const cbIP_IPv6Settings * const IPv6Settings, cbIP_interfaceSettings const * const ifConfig, cbIP_statusIndication callback, void* callbackArg)
 {
     struct ip_addr ipaddr;
     struct ip_addr netmask;
@@ -163,91 +184,99 @@ void cbIP_initWlanInterfaceDHCP(char* hostname, const cbIP_IPv6Settings * const 
 
     memcpy(&ip6addr, &IPv6Settings->linklocal.value, sizeof(ip6addr));
 
-    memcpy(&wlanIf.ifConfig, ifConfig, sizeof(wlanIf.ifConfig));
+    memcpy(&panIf.ifConfig, ifConfig, sizeof(panIf.ifConfig));
 
-    netif_add(&wlanIf.hInterface, &ipaddr, &netmask, &gw, &wlanIf, cb_netif_init, ethernet_input);
-    wlanIf.hInterface.hostname = hostname;
-    wlanIf.callbackArg = callbackArg;
-    wlanIf.hInterface.ip6_autoconfig_enabled = 0;
+    netif_add(&panIf.hInterface, &ipaddr, &netmask, &gw, &panIf, cb_netif_init, ethernet_input);
+    panIf.hInterface.hostname = hostname;
+    panIf.callbackArg = callbackArg;
+    panIf.hInterface.ip6_autoconfig_enabled = 0;
     if ((ip6addr.addr[0] == 0) && (ip6addr.addr[1] == 0) &&
         (ip6addr.addr[2] == 0) && (ip6addr.addr[3] == 0))
     {
-        netif_create_ip6_linklocal_address(&wlanIf.hInterface, 1);
+        netif_create_ip6_linklocal_address(&panIf.hInterface, 1);
     }
     else
     {
-        wlanIf.hInterface.ip6_addr[0] = ip6addr;
+        panIf.hInterface.ip6_addr[0] = ip6addr;
     }
-    netif_ip6_addr_set_state((&wlanIf.hInterface), 0, IP6_ADDR_TENTATIVE);
+    netif_ip6_addr_set_state((&panIf.hInterface), 0, IP6_ADDR_TENTATIVE);
 
-    wlanIf.statusCallback = callback;
-    netif_set_status_callback(&wlanIf.hInterface, netif_status_callback);
+    panIf.statusCallback = callback;
+    netif_set_status_callback(&panIf.hInterface, netif_status_callback);
 
     LWIP_PRINT("Using DHCP\n");
-    dhcp_start(&wlanIf.hInterface);
+    dhcp_start(&panIf.hInterface);
+
+    cb_uint32 result;
+    result = cbBTPAN_registerDataCallback(&_panCallBack);
+    cb_ASSERT(result == cbBTPAN_RESULT_OK);
 }
 
-void cbIP_removeWlanInterface(void)
+void cbIP_removePanInterface(void)
 {
     LWIP_PRINT("Interface down\n");
 
-    cbWLAN_deregisterStatusCallback(statusIndication, &wlanIf.hInterface);
-    dhcp_stop(&wlanIf.hInterface);
-    netif_remove(&wlanIf.hInterface);
-    dhcp_cleanup(&wlanIf.hInterface);
+    // TODO deregister PAN callbacks
+
+    dhcp_stop(&panIf.hInterface);
+    netif_remove(&panIf.hInterface);
+    dhcp_cleanup(&panIf.hInterface);
 }
 
 /*===========================================================================
  * INTERNAL FUNCTIONS
  *=========================================================================*/
 
-static void statusIndication(void *callbackContext, cbWLAN_StatusIndicationInfo status, void *data)
+static void handleConnectEvt(cbBCM_Handle connHandle, cbBCM_ConnectionInfo info)
 {
-    (void)data;
-    struct netif* netif = (struct netif*)callbackContext;
+    printf("%s\n",__FUNCTION__);
 
-    switch (status) {
-        case cbWLAN_STATUS_STOPPED:
-        case cbWLAN_STATUS_ERROR:
-        case cbWLAN_STATUS_DISCONNECTED:
-        case cbWLAN_STATUS_CONNECTION_FAILURE:
-        case cbWLAN_STATUS_CONNECTING:
-            netif_set_link_down(netif);
-            break;
-        case cbWLAN_STATUS_CONNECTED:
-        case cbWLAN_STATUS_AP_STA_ADDED:
-            netif_set_link_up(netif);
-            break;
-        default:
-            break;
-    }
+    struct netif* netif = &panIf.hInterface;
+    netif_set_link_up(netif);
 }
 
-static void packetIndication(void *callbackContext, cbWLAN_PacketIndicationInfo *packetInfo)
+static void handleDisconnectEvt(cbBCM_Handle connHandle)
 {
-    struct netif* netif = (struct netif*)callbackContext;
-    struct pbuf* pbuf = (struct pbuf*)packetInfo->rxData;
+    printf("%s\n",__FUNCTION__);
 
-    cb_ASSERT(netif != NULL);
+    struct netif* netif = &panIf.hInterface;
+    netif_set_link_down(netif);
+}
+
+static void handleDataEvt(cbBTPAN_Handle btPanHandle, cb_uint8* pData, cb_uint16 length)
+{
+    printf("%s\n",__FUNCTION__);
+
+    struct pbuf* pbuf;
+    struct netif* netif = &panIf.hInterface;
+
+    pbuf = (struct pbuf*)cbIP_allocDataFrame(length);
     cb_ASSERT(pbuf != NULL);
+    cb_boolean status = cbIP_copyToDataFrame((cbIP_frame*)pbuf,pData,length,0);
+    cb_ASSERT(status);
 
-    wlanIf.statusCallback(cbIP_NETWORK_ACTIVITY, NULL, NULL, wlanIf.callbackArg);
+    panIf.statusCallback(cbIP_NETWORK_ACTIVITY, NULL, NULL, panIf.callbackArg);
     netif->input(pbuf, netif);
 
     LINK_STATS_INC(link.recv);
 }
 
+static void handleDataCnf(cbBTPAN_Handle btPanHandle, cb_int32 result)
+{
+    printf("%s\n",__FUNCTION__);
+}
+
 static err_t cb_netif_init(struct netif* netif)
 {
-    cbIP_wlanIf* hIf;
+    cbIP_panIf* hIf;
 
     netif->name[0] = IFNAME0;
     netif->name[1] = IFNAME1;
     netif->num = 0;
-    netif->output = wlanif_output_ipv4;
-    netif->output_ip6 = wlanif_output_ipv6;
+    netif->output = panif_output_ipv4;
+    netif->output_ip6 = panif_output_ipv6;
     netif->linkoutput = low_level_output;
-    hIf = (cbIP_wlanIf*)netif->state;
+    hIf = (cbIP_panIf*)netif->state;
 
     netif->hwaddr_len = 6;
 
@@ -262,9 +291,6 @@ static err_t cb_netif_init(struct netif* netif)
 
     netif->flags = NETIF_FLAG_BROADCAST | NETIF_FLAG_ETHARP | NETIF_FLAG_IGMP;
 
-    cbWLAN_registerStatusCallback(statusIndication, &hIf->hInterface);
-    cbWLAN_registerPacketIndicationCallback(packetIndication, &hIf->hInterface);
-
     return ERR_OK;
 }
 
@@ -272,32 +298,51 @@ static err_t cb_netif_init(struct netif* netif)
  * lwIP interface functions
  *-------------------------------------------------------------------------*/
 
-static err_t wlanif_output_ipv4(struct netif* netif, struct pbuf* p, struct ip_addr* ipaddr)
+static err_t panif_output_ipv4(struct netif* netif, struct pbuf* p, struct ip_addr* ipaddr)
 {
     /* resolve hardware address, then send (or queue) packet */
     return etharp_output(netif, p, ipaddr);
 }
 
-static err_t wlanif_output_ipv6(struct netif *netif, struct pbuf *p, ip6_addr_t *ipaddr)
+static err_t panif_output_ipv6(struct netif *netif, struct pbuf *p, ip6_addr_t *ipaddr)
 {
     return ethip6_output(netif, p, ipaddr);
 }
 
 static err_t low_level_output(struct netif* netif, struct pbuf* p)
 {
+    err_t retVal = ERR_CONN;
+
     if (netif_is_link_up(netif)) {
         pbuf_ref(p);
-        wlanIf.statusCallback(cbIP_NETWORK_ACTIVITY, NULL, NULL, wlanIf.callbackArg);
-        cbWLAN_sendPacket(p);
+        panIf.statusCallback(cbIP_NETWORK_ACTIVITY, NULL, NULL, panIf.callbackArg);
 
-        LINK_STATS_INC(link.xmit);
+        cb_uint32 totSize = cbIP_getDataFrameSize(p);
+        UAllocTraits_t t;
+        t.flags = 0;
+        t.extended = 0;
+        cb_uint8* buf = mbed_ualloc(totSize,t);
+        cb_ASSERT(buf != NULL); // Throw away packets if we can not allocate?
+        cb_boolean status = cbIP_copyFromDataFrame(buf,p,totSize,0);
+        cb_ASSERT(status);
+        cb_int32 result = cbBTPAN_reqData(1,buf,totSize); // TODO don't hardcode hadle
+        if(result == cbBTPAN_RESULT_OK)
+        {
+            retVal = ERR_OK;
+            LINK_STATS_INC(link.xmit);
+        }
+        else
+        {
+            printf("low_level_output - packet dropped\n");
+        }
+        mbed_ufree(buf);
 
-        return ERR_OK;
+        return retVal;
     }
 
     LINK_STATS_INC(link.drop);
 
-    return ERR_CONN;
+    return retVal;
 }
 
 static void netif_status_callback(struct netif *netif)
@@ -315,8 +360,8 @@ static void netif_status_callback(struct netif *netif)
     memcpy(&ipV6Settings.linklocal.value, netif->ip6_addr[0].addr, sizeof (cbIP_IPv6Address));
 
     if (netif->flags & NETIF_FLAG_UP) {
-        wlanIf.statusCallback(cbIP_NETWORK_UP, wlanIf.callbackArg, &ipV4Settings, &ipV6Settings);
+        panIf.statusCallback(cbIP_NETWORK_UP, panIf.callbackArg, &ipV4Settings, &ipV6Settings);
     } else {
-        wlanIf.statusCallback(cbIP_NETWORK_DOWN, wlanIf.callbackArg, &ipV4Settings, &ipV6Settings);
+        panIf.statusCallback(cbIP_NETWORK_DOWN, panIf.callbackArg, &ipV4Settings, &ipV6Settings);
     }
 }
